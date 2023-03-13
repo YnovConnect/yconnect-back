e72b3393a1ac6aa4ede2ef069eaf16079da0b4aa
PikkMoune71 <antoinepicard385@gmail.com>
2023-03-13 22:20:17 +0100

    create websocket

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ import config from "./src/config/index.js";
 import cors from "@koa/cors";
 import isAuthenticated from "./src/middleware/isAuthentificated.js";
 import swaggerUi from "swagger-ui-koa";
-import convert from 'koa-convert';
-import swaggerJSDoc from 'swagger-jsdoc';
-import mount from 'koa-mount';
+import convert from "koa-convert";
+import swaggerJSDoc from "swagger-jsdoc";
+import mount from "koa-mount";
+import websockify from "koa-websocket";
+import WebSocket from "ws";
 
-const app = new Koa();
+const app = websockify(new Koa());
+const ws = new WebSocket("ws://localhost:3000");
 
 // Initialisation de Mongoose avec la configuration de connexion
 mongoose.connect(config.mongoUri, {
@@ -38,26 +41,39 @@ app.use(
 );
 
 const options = {
-    swaggerDefinition: {
-        info: {
-            title: 'API', // Title (required)
-            version: '2.0.0', // Version (required)
-        },
+  swaggerDefinition: {
+    info: {
+      title: "API", // Title (required)
+      version: "2.0.0", // Version (required)
     },
-    apis: [
-        './src/routes/*.js', // Path to the API docs from root
-    ],
+  },
+  apis: [
+    "./src/routes/*.js", // Path to the API docs from root
+  ],
 };
 // Initialize swagger-jsdoc -> returns validated swagger spec in json format
 const swaggerSpec = swaggerJSDoc(options);
 app.use(swaggerUi.serve); //serve swagger static files
-app.use(convert(mount('/swagger', swaggerUi.setup(swaggerSpec))));
-
+app.use(convert(mount("/swagger", swaggerUi.setup(swaggerSpec))));
 
 app.use(APIRouter.allowedMethods());
 app.use(isAuthenticated);
 app.use(APIRouter.routes());
 
+ws.on("message", (msg) => {
+  console.log("Received message:", msg.toString());
+});
+
+ws.on("close", () => {
+  console.log("Disconnected from server");
+});
+
+app.ws.use((ctx, next) => {
+  console.log("A user connected");
+  ctx.websocket.send("Welcome!");
+
+  return next();
+});
+
 // Démarrage du serveur sur le port 3000
 app.listen(3000, () => console.log("Le serveur écoute sur le port 3000"));
-//post créer une entité  -- put ou patch modifie
